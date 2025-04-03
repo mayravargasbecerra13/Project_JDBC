7218e26aaf77f0abcad514c992222a9366f0d99f
Mayra Vargas <vargasmayra800@gmail.com>
2025-04-03 08:14:00 -0500

    Se inicializo el projecto

---
+++ src/main/java/com/mycompany/projec_jdbc/Projec_JDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projec_jdbc;
+
+/**
+ *
+ * @author Personal
+ */
+public class Projec_JDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
